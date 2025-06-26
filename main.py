a0d0d776895b7af97c227a166d3231be9c7a20bd
r1t35h <46487008+r1t35h@users.noreply.github.com>
2025-06-26 14:16:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allow Vite dev server and deployed frontend
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
